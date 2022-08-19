f2476a0bae1aa25fad1c74cf77662b618df756fd
shu <shu.takita03@gmail.com>
2022-08-19 16:44:53 +0900

    ログアウトの修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
+  devise_for :users
   resources :users do
     member do
       get :following, :followers
@@ -8,8 +9,6 @@ Rails.application.routes.draw do
   resources :relationships,       only: [:create, :destroy]
   get 'home/index'
 
-  devise_for :users
-  resources :users
   resources :tasks
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
