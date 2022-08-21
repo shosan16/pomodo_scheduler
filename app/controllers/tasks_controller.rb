9243a4ce4e02460a8e25809603cb2b242880b37e
shosan16 <30305829kasukabe@gmail.com>
2022-08-21 15:35:02 +0900

    タスクをDBに登録するためのcreateアクションを実装

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,8 +1,19 @@
 class TasksController < ApplicationController
   def new
-    @task = Task.new
+    @task = current_user.tasks.build if user_signed_in?
   end
 
   def create
+    @task = current_user.tasks.build if user_signed_in?
+    if @task.save
+      redirect_to root_path
+    else
+      render 'new'
+    end
   end
+
+  private
+    def task_params
+      params.permit(:task_name, :task_time, :rest_time, :set_number)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,6 @@ Rails.application.routes.draw do
   resources :relationships,       only: [:create, :destroy]
   get 'home/index'
 
-  resources :tasks
-  
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get "tasks/new", to:'tasks#new'
+  post "tasks/new", to:'tasks#create'
 end
