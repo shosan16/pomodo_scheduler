7e87edda8a3bcb1f1eae6ba26b288ce8613d673b
shosan16 <30305829kasukabe@gmail.com>
2022-08-22 17:13:49 +0900

    今日のタスクだけを表示

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,6 @@
 class StaticPagesController < ApplicationController
   def home
     @user = current_user
+    @tasks = current_user.tasks.where(created_at: Time.zone.today.beginning_of_day..Time.zone.today.end_of_day)
   end
 end
