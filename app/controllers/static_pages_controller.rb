d241407e0ad2f3282fcc0a474bf07a4ddbd7e16d
shosan16 <30305829kasukabe@gmail.com>
2022-08-23 16:10:54 +0900

    ログアウトボタンを押すとエラーが出る→解消

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,8 @@
 class StaticPagesController < ApplicationController
   def home
-    @user = current_user
-    # @tasks = current_user.tasks
-    @tasks = current_user.tasks.where(created_at: Time.zone.today.beginning_of_day..Time.zone.today.end_of_day)
+    if user_signed_in?
+      @user = current_user
+      @tasks = current_user.tasks.where(created_at: Time.zone.today.beginning_of_day..Time.zone.today.end_of_day)
+    end
   end
 end
