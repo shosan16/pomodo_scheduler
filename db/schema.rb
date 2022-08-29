7a4d05c5d6978ad0b268412398cdd731799f4c93
shosan16 <30305829kasukabe@gmail.com>
2022-08-29 17:15:55 +0900

    seedデータ変更

--- db/schema.rb
+++ db/schema.rb
@@ -10,44 +10,45 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_220_825_073_627) do
+ActiveRecord::Schema.define(version: 2022_08_25_073627) do
+
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+  enable_extension "plpgsql"
 
-  create_table 'relationships', force: :cascade do |t|
-    t.integer 'follower_id'
-    t.integer 'followed_id'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.index ['followed_id'], name: 'index_relationships_on_followed_id'
-    t.index %w[follower_id followed_id], name: 'index_relationships_on_follower_id_and_followed_id', unique: true
-    t.index ['follower_id'], name: 'index_relationships_on_follower_id'
+  create_table "relationships", force: :cascade do |t|
+    t.integer "follower_id"
+    t.integer "followed_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["followed_id"], name: "index_relationships_on_followed_id"
+    t.index ["follower_id", "followed_id"], name: "index_relationships_on_follower_id_and_followed_id", unique: true
+    t.index ["follower_id"], name: "index_relationships_on_follower_id"
   end
 
-  create_table 'tasks', force: :cascade do |t|
-    t.string 'name'
-    t.integer 'task_time'
-    t.integer 'rest_time'
-    t.integer 'set_number'
-    t.bigint 'user_id', null: false
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.index ['user_id'], name: 'index_tasks_on_user_id'
+  create_table "tasks", force: :cascade do |t|
+    t.string "name"
+    t.integer "task_time"
+    t.integer "rest_time"
+    t.integer "set_number"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_tasks_on_user_id"
   end
 
-  create_table 'users', force: :cascade do |t|
-    t.string 'email', default: '', null: false
-    t.string 'encrypted_password', default: '', null: false
-    t.string 'reset_password_token'
-    t.datetime 'reset_password_sent_at'
-    t.datetime 'remember_created_at'
-    t.datetime 'created_at', precision: 6, null: false
-    t.datetime 'updated_at', precision: 6, null: false
-    t.string 'name'
-    t.string 'avatar'
-    t.index ['email'], name: 'index_users_on_email', unique: true
-    t.index ['reset_password_token'], name: 'index_users_on_reset_password_token', unique: true
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.string "name"
+    t.string "avatar"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-  add_foreign_key 'tasks', 'users'
+  add_foreign_key "tasks", "users"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,89 +1,99 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+# ユーザーの一部を対象にタスクを生成する（昨日)
+users = User.order(:id).take(20)
+1.times do
+  name = 'レポート'
+  task_time = 20
+  rest_time = 1
+  set_number = Faker::Number.between(from: 1, to: 3).to_i
+  created_at = '2022-08-25 09:05:31'
+  users.each do |user|
+    user.tasks.create!(name: name, task_time: task_time, rest_time: rest_time, set_number: set_number,
+                       created_at: created_at)
+  end
+end
+
+# ユーザーの一部を対象にタスクを生���する（昨日)
+users = User.order(:id).take(20)
+1.times do
+  name = '英語'
+  task_time = 20
+  rest_time = 1
+  set_number = Faker::Number.between(from: 1, to: 3).to_i
+  created_at = '2022-08-25 09:05:31'
+  users.each do |user|
+    user.tasks.create!(name: name, task_time: task_time, rest_time: rest_time, set_number: set_number,
+                       created_at: created_at)
+  end
+end
 
 # ユーザーの一部を対象にタスクを生成する（昨日)
-# users = User.order(:id).take(20)
-# 1.times do
-#   name = "レポート"
-#   task_time = 20
-#   rest_time = 1
-#   set_number = Faker::Number.between(from: 1, to: 3).to_i
-#   created_at = "2022-08-25 09:05:31"
-#   users.each { |user| user.tasks.create!(name: name, task_time: task_time, rest_time: rest_time, set_number: set_number, created_at:created_at) }
-# end
-
-# # ユーザーの一部を対象にタスクを生成する（昨日)
-# users = User.order(:id).take(20)
-# 1.times do
-#   name = "英語"
-#   task_time = 20
-#   rest_time = 1
-#   set_number = Faker::Number.between(from: 1, to: 3).to_i
-#   created_at = "2022-08-25 09:05:31"
-#   users.each { |user| user.tasks.create!(name: name, task_time: task_time, rest_time: rest_time, set_number: set_number, created_at:created_at) }
-# end
-
-# # ユーザーの一部を対象にタスクを生成する（昨日)
-# users = User.order(:id).take(20)
-# 1.times do
-#   name = "英語"
-#   task_time = 20
-#   rest_time = 1
-#   set_number = Faker::Number.between(from: 1, to: 3).to_i
-#   created_at = "2022-08-25 09:05:31"
-#   users.each { |user| user.tasks.create!(name: name, task_time: task_time, rest_time: rest_time, set_number: set_number, created_at:created_at) }
-# end
+users = User.order(:id).take(20)
+1.times do
+  name = '英語'
+  task_time = 20
+  rest_time = 1
+  set_number = Faker::Number.between(from: 1, to: 3).to_i
+  created_at = '2022-08-25 09:05:31'
+  users.each do |user|
+    user.tasks.create!(name: name, task_time: task_time, rest_time: rest_time, set_number: set_number,
+                       created_at: created_at)
+  end
+end
 
 # ユーザー
-# User.create!(name:  "Example User",
-#              email: "example@sakigake.com",
-#              password:              "foobar",
-#              password_confirmation: "foobar")
+User.create!(name: 'Example User',
+             email: 'example@sakigake.com',
+             password: 'foobar',
+             password_confirmation: 'foobar')
 
-# 49.times do |n|
-#   name  = Faker::Name.name
-#   email = "example-#{n+1}@sakigake.com"
-#   password = "password"
-#   User.create!(name:  name,
-#                email: email,
-#                password:              password,
-#                password_confirmation: password)
-# end
+49.times do |n|
+  name  = Faker::Name.name
+  email = "example-#{n + 1}@sakigake.com"
+  password = 'password'
+  User.create!(name: name,
+               email: email,
+               password: password,
+               password_confirmation: password)
+end
 
 # 以下のリレーションシップを作成する
-# users = User.all
-# user  = users.first
-# following = users[2..30]
-# followers = users[3..20]
-# following.each { |followed| user.follow(followed) }
-# followers.each { |follower| follower.follow(user) }
+users = User.all
+user  = users.first
+following = users[2..30]
+followers = users[3..20]
+following.each { |followed| user.follow(followed) }
+followers.each { |follower| follower.follow(user) }
 
 # 過去にやったタスク8/1〜8/24
-# users = User.order(:id).take(30)
-# 15.times do
-#   name = "プログラミング"
-#   rest_time = 1
-#   users.each { |user| user.tasks.create!(name: name, task_time: rand(10..15), rest_time: rest_time, set_number: rand(1..4), created_at:Faker::Date.between(from: '2022-08-01', to: '2022-08-24')) }
-# end
+users = User.order(:id).take(30)
+15.times do
+  name = 'プログラミング'
+  rest_time = 1
+  users.each do |user|
+    user.tasks.create!(name: name, task_time: rand(10..15), rest_time: rest_time, set_number: rand(1..4),
+                       created_at: Faker::Date.between(from: '2022-08-01', to: '2022-08-24'))
+  end
+end
 
-# users = User.order(:id).take(30)
-# 15.times do
-#   name = "英語"
-#   rest_time = 1
-#   users.each { |user| user.tasks.create!(name: name, task_time: rand(10..15), rest_time: rest_time, set_number: rand(1..4), created_at:Faker::Date.between(from: '2022-08-01', to: '2022-08-24')) }
-# end
+users = User.order(:id).take(30)
+15.times do
+  name = '英語'
+  rest_time = 1
+  users.each do |user|
+    user.tasks.create!(name: name, task_time: rand(10..15), rest_time: rest_time, set_number: rand(1..4),
+                       created_at: Faker::Date.between(from: '2022-08-01', to: '2022-08-24'))
+  end
+end
 
-# users = User.order(:id).take(30)
-# 15.times do
-#   name = "レポート"
-#   rest_time = 1
-#   users.each { |user| user.tasks.create!(name: name, task_time: rand(10..15), rest_time: rest_time, set_number: rand(1..4), created_at:Faker::Date.between(from: '2022-08-01', to: '2022-08-24')) }
-# end
+users = User.order(:id).take(30)
+15.times do
+  name = 'レポート'
+  rest_time = 1
+  users.each do |user|
+    user.tasks.create!(name: name, task_time: rand(10..15), rest_time: rest_time, set_number: rand(1..4),
+                       created_at: Faker::Date.between(from: '2022-08-01', to: '2022-08-24'))
+  end
+end
 
 # 今日やったタスク
 users = User.order(:id).take(30)
