235b97d0d6aa7687da1ca7b9f489a12f8dd2ccc5
shota <30305829kasukabe@gmail.com>
2022-09-22 14:22:18 +0900

    ゲストユーザーの名前を設定

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -14,8 +14,9 @@ class StaticPagesController < ApplicationController
   end
 
   def guest_sign_in
-    user = User.find_or_create_by!(email: 'guest@example.com') do |user|
+    user = User.find_or_create_by!(email: 'guest@example.com', name: 'ゲストユーザー') do |user|
       user.password = SecureRandom.urlsafe_base64
+      user.name = 'ゲストユーザー'
     end
     sign_in user
     redirect_to root_path, notice: 'ゲストユーザーとしてログインしました。'
